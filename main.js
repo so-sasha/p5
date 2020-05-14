a6c718d8fd7f5b522fe6f462333f5a7e8e9e28a2
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-05-15 01:06:44 +0300

    menu

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('menu').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('technology').style.display = 'block';
+  document.getElementById('works').style.display = 'block';
+}
