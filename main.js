4c2852413149c0512fb834200322900b7c3e62ba
so-sasha <55274688+so-sasha@users.noreply.github.com>
2020-05-19 13:21:53 +0300

    burger menu

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ document.getElementById('menu').onclick = e => {
   document.getElementById('technology').style.display = 'block';
   document.getElementById('works').style.display = 'block';
 }
+
+document.getElementById('burger').onclick = e => {
+  e.target.style.display = 'none';
+  document.getElementById('technology').style.display = 'block';
+  document.getElementById('works').style.display = 'block';
+}
